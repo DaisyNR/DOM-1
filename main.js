5866c50e36faf999d26efd7a84ca00318f440397
Daisy Nardella <daisy50@live.it>
2025-03-27 00:25:24 +0100

    terminato dom1

---
+++ main.js
@@ -0,0 +1,30 @@
+let p = document.querySelectorAll('p')
+let secondParagraph = document.querySelector('#secondParagraph');
+let thirdParagraph = document.querySelector('#thirdParagraph');
+let colorBtn = document.querySelector('#color-btn');
+let strongBtn = document.querySelector('#strong-btn');
+let vanishBtn = document.querySelector('#vanish-btn');
+
+colorBtn.addEventListener('click', ()=>{
+    p.forEach(p => {
+        let r = Math.floor(Math.random() * 255)
+        let g = Math.floor(Math.random() * 255)
+        let b = Math.floor(Math.random() * 255)
+        p.style.color = `rgb(${r}, ${g}, ${b})`
+    });
+});
+
+
+strongBtn.addEventListener('click', ()=>{
+    secondParagraph.classList.toggle('font-custom');
+    // strongBtn.innerText = 'Torna indietro'  
+});
+
+vanishBtn.addEventListener('click', ()=>{
+    thirdParagraph.classList.toggle('display')
+})
+
+
+
+
+
